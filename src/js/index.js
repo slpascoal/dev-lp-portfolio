d57222f9281f4da2848e98d59bb6c5006d6467f6
Silas L. Pascoal <slpascoal01@gmail.com>
2024-04-07 15:44:34 -0300

    Criando meu portfolio
    Criando meu repositório e adicionando meu portfolio para subir na web.

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+
+    removerBotao();
+});
+
+function removerBotao() {
+    botaoMostrarProjetos.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetosInativo => {
+        projetosInativo.classList.add('ativo');
+    });
+}
